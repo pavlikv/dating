6cd819de3ad69ea8f40f69220e936c29606b20e6
Pavel Vashchuk <pavelvashchuk@gmail.com>
2018-02-01 17:43:09 -0500

    Added bootstrap to all pages

---
+++ model/validate.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pavel
+ * Date: 1/31/18
+ * Time: 7:55 PM
+ */
+
+function validName() {
+
+}
+
+function validAge() {
+
+}
+
+function validPhone(){
+
+}
+
+function validOutdoor() {
+
+}
+
+function validIndoor() {
+
+}
