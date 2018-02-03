4f2cad404af628d96cf4f20fc0b5588b3848da72
Pavel Vashchuk <pavelvashchuk@gmail.com>
2018-02-02 19:38:33 -0500

    created validation functions

--- index.php
+++ index.php
@@ -30,10 +30,12 @@ $f3->route('POST|GET /', function() {
 
 
 //define a route
-$f3->route('GET|POST /form1', function() {
+$f3->route('GET|POST /form1', function($f3) {
+
 
     $template = new Template();
     echo $template->render('pages/form1.html');
+
 
 
 }
@@ -41,42 +43,65 @@ $f3->route('GET|POST /form1', function() {
 
 //define a route
 $f3->route('GET|POST /form2', function() {
+    if(isset($_POST['submit'])) {
 
+        $_SESSION['firstName'] = $_POST['firstName'];
+        $_SESSION['lastName'] = $_POST['lastName'];
+        $_SESSION['age'] = $_POST['age'];
+        $_SESSION['gender'] = $_POST['gender'];
+        $_SESSION['phone'] = $_POST['phone'];
+
+        //include ('model/validate.php');
+
+    }
 
     $template = new Template();
     echo $template->render('pages/form2.html');
-    $_SESSION['firstName'] = $_POST['firstName'];
-    $_SESSION['lastName'] = $_POST['lastName'];
-    $_SESSION['age'] = $_POST['age'];
-    $_SESSION['gender'] = $_POST['gender'];
-    $_SESSION['phone'] = $_POST['phone'];
+
+
 }
 );
 
 //define a route
 $f3->route('GET|POST /form3', function() {
+    /*
+    if(isset($_POST['submit'])) {
+        $_SESSION['email'] = $_POST['email'];
+        $_SESSION['state'] = $_POST['state'];
+        $_SESSION['seeking'] = $_POST['seeking'];
+        $_SESSION['bio'] = $_POST['bio'];
+    }*/
 
     $template = new Template();
     echo $template->render('pages/form3.html');
-    $_SESSION['email'] = $_POST['email'];
-    $_SESSION['state'] = $_POST['state'];
-    $_SESSION['seeking'] = $_POST['seeking'];
-    $_SESSION['bio'] = $_POST['bio'];
+
 
 }
 );
 
 //define a route
 $f3->route('GET|POST /formSummary', function($f3) {
+    /*
+    if(isset($_POST['submit'])) {
+        $_SESSION['indoors'] = $_POST['indoors'];
+        $_SESSION['outdoors'] = $_POST['outdoors'];
+    }
 
-    $_SESSION['indoor'] = $_POST['indoor'];
-    echo "<h1>".$_SESSION['indoor']."</h1>";
-    //$f3->set('indoor', $_SESSION['indoor']);
+    $f3->set('outdoors', $_SESSION['outdoors']);
+    $f3->set('indoors', $_SESSION['indoors']);
+    */
 
     $f3->set('firstName', $_SESSION['firstName']);
     $f3->set('lastName', $_SESSION['lastName']);
+    $f3->set('age', $_SESSION['age']);
+    $f3->set('gender', $_SESSION['gender']);
     $f3->set('phone', $_SESSION['phone']);
-    $f3->set('phone', $_SESSION['phone']);
+    /*
+    $f3->set('email', $_SESSION['email']);
+    $f3->set('state', $_SESSION['state']);
+    $f3->set('seeking', $_SESSION['seeking']);
+    $f3->set('bio', $_SESSION['bio']);
+    */
 
     $template = new Template();
     echo $template->render('pages/formSummary.html');
--- model/validate.php
+++ model/validate.php
@@ -6,22 +6,34 @@
  * Time: 7:55 PM
  */
 
-function validName() {
-
+function validName($name) {
+    return ctype_alpha($name);
 }
 
-function validAge() {
-
+function validAge($age) {
+    return (is_numeric($age) && $age >= 18);
 }
 
-function validPhone(){
-
+function validPhone($phone){
+    return preg_match("/^[0-9]{3}-[0-9]{4}-[0-9]{4}$/", $phone);
 }
 
-function validOutdoor() {
-
+function validOutdoor($listOfOutdoor) {
+    $validList = array('hiking', 'biking', 'swimming', 'collecting', 'walking', 'climbing');
+    foreach ($listOfOutdoor as $activity) {
+        if (!in_array($activity, $validList)) {
+            return false;
+        }
+    }
+    return true;
 }
 
-function validIndoor() {
-
+function validIndoor($listOfIndoor) {
+    $validList = array('tv', 'movies', 'cooking', 'board games', 'puzzles', 'reading', 'playing cards', 'video games');
+    foreach ($listOfIndoor as $activity) {
+        if (!in_array($activity, $validList)) {
+            return false;
+        }
+    }
+    return true;
 }
