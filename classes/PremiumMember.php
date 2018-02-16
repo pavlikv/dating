97ba6bad02a41ebfa0ff78d88a60201e60318879
Pavel Vashchuk <pavelvashchuk@gmail.com>
2018-02-16 02:17:41 -0500

    Phpdoc on PremiumMember class

--- classes/PremiumMember.php
+++ classes/PremiumMember.php
@@ -6,23 +6,47 @@
  * Time: 11:15 AM
  */
 
+/**
+ * Class PremiumMember extends Member.
+ * Class is used when a user checks the premium member option.
+ * Otherwise will use the Member class.
+ */
 class PremiumMember extends Member
 {
+
 
     private $_inDoorInterests = array();
     private $_outDoorInterests = array();
 
 
+    /**
+     * getter method
+     * @return array of Indoor interests
+     */
     function getInDoorInterests() {
         return $this->_inDoorInterests;
     }
+
+    /**
+     * Setter method, to set an array of indoor interests
+     * @param $array of interests
+     */
     function setInDoorInterests($array){
         $this->_inDoorInterests = $array;
     }
 
+    /**
+     * getter method
+     * @return array of outdoor interests
+     */
     function getOutDoorInterests() {
         return $this->_outDoorInterests;
     }
+
+    /**
+     * Setter method, to set an array of outdoor interests
+     * @param $array of interests
+     */
     function setOutDoorInterests($array){
         $this->_outDoorInterests = $array;
     }
